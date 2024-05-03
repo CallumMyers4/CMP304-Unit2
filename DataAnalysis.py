fac47266f16144a94742b392b0b71ae9b9a7b9ba
CallumMyers4 <callummyers4@gmail.com>
2024-05-03 01:50:24 +0100

    Tried adding elbow check
    issue with kMeans

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -2,29 +2,29 @@ def maxValues(players, matches):
     total_goals_pp = players['core_goals'].sum()
     avg_goals = total_goals_pp / matches
     most_goals = total_goals_pp.max()
-    print("Most goals: ", most_goals)
-    print(avg_goals)
+    norm_goals = avg_goals / most_goals
+    print(norm_goals)
 
     total_assists_pp = players['core_assists'].sum()
     avg_assists = total_assists_pp / matches
     most_assists = total_assists_pp.max()
-    print("Most assists: ", most_assists)
-    print(avg_assists)
+    norm_assists = avg_assists / most_assists
+    print(norm_assists)
 
     total_saves_pp = players['core_saves'].sum()
     avg_saves = total_saves_pp / matches
     most_saves = total_saves_pp.max()
-    print("Most saves: ", most_saves)
-    print(avg_saves)
+    norm_saves = avg_saves / most_saves
+    print (norm_saves)
 
     total_demos = players['demo_inflicted'].sum()
     avg_demos = total_demos / matches
     most_demos = total_demos.max()
-    print("Most demos: ", most_demos)
-    print(avg_demos)
+    norm_demos = avg_demos / most_demos
+    print (norm_demos)
 
     total_time_supersonic_pp = players['movement_percent_supersonic_speed'].sum()
-    avg_time_spent_supersonic = total_time_supersonic_pp / matches
+    avg_time_supersonic = total_time_supersonic_pp / matches
     most_time_supersonic = total_time_supersonic_pp.max()
-    print("Most percent of time supersonic: ", most_time_supersonic)
-    print(avg_time_spent_supersonic)
+    norm_time_supersonic = avg_time_supersonic / most_time_supersonic
+    print (norm_time_supersonic)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import DataPreparation as dataPrep
 import DataAnalysis as dataAnalysis
+from sklearn.linear_model import LinearRegression
+from sklearn.cluster import KMeans 
 
 region_check = input("Enter region to classify: ")
 dataPrep.matchesPlayed()
@@ -11,3 +13,13 @@ player_data = region_data.groupby('player_id')   #gets the average per game by f
 matches_played = player_data.size()     #the size of the player_data variable will show how many matches each player participated in - 
                                         #therefore can be useful for averages
 dataAnalysis.maxValues(player_data, matches_played)
+
+#perform a elbow check to determine the best cluster value
+elbowCheck = 12 #max number of clusters to check
+wss = []
+
+for e in range(1, elbowCheck + 1):
+    # Run KMeans for the current number of clusters
+    kmeans = KMeans(n_clusters=e, random_state=0, n_init="auto").fit(player_data[['core_goals']]) 
+    # Append the inertia_ value to wss
+    wss.append(kmeans.inertia_)
