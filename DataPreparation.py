9587633facc87a4a9015e40d7bc0e722633f66c3
CallumMyers4 <callummyers4@gmail.com>
2024-05-06 06:07:19 +0100

    Added elbow check to get ideal clusters
    Elbow seems to be starting to flatten at around 120 clusters

--- DataPreparation.py
+++ DataPreparation.py
@@ -1,13 +1,31 @@
 import numpy
 import pandas
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans 
 
 #calculate number of matches played per player
-def matchesPlayed():
+def readData():
     #read in file
     playerDataMatch = "rl_data/matches_by_players.csv"
     global playersData 
     playersData = pandas.read_csv(playerDataMatch)
+
+def plotElbow():
+    elbowCheck = 150 #max number of clusters to check
+    wss = []
+
+    for e in range(1, elbowCheck + 1):
+        #creates a new dataset suitable for the elbow check using only numerical required data and filling in empty gaps with 0
+        filled_data = playersData[['core_goals', 'core_assists', 'core_saves', 'demo_inflicted', 'movement_percent_supersonic_speed']].fillna(0)
+        # Run KMeans for the current number of clusters
+        kmeans = KMeans(n_clusters=e, random_state=0, n_init="auto").fit(filled_data) 
+        # Append the inertia_ value to wss
+        wss.append(kmeans.inertia_)
+
+    print("Plotting graph...")
+    plt.plot(wss) 
+    plt.show()
+    print("Graph plotted.")
 
 def selectRegion(region):
     player_data = playersData[playersData['team_region'] == region]
--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import DataAnalysis as dataAnalysis
 from sklearn.linear_model import LinearRegression
 from sklearn.cluster import KMeans 
 
+dataPrep.readData()
+dataPrep.plotElbow()
 region_check = input("Enter region to classify: ")
-dataPrep.matchesPlayed()
 region_data = dataPrep.selectRegion(region_check)
 
 player_data = region_data.groupby('player_id')   #gets the average per game by finding the sum of all values and dividing by the number of 
@@ -13,13 +14,3 @@ player_data = region_data.groupby('player_id')   #gets the average per game by f
 matches_played = player_data.size()     #the size of the player_data variable will show how many matches each player participated in - 
                                         #therefore can be useful for averages
 dataAnalysis.maxValues(player_data, matches_played)
-
-#perform a elbow check to determine the best cluster value
-elbowCheck = 12 #max number of clusters to check
-wss = []
-
-for e in range(1, elbowCheck + 1):
-    # Run KMeans for the current number of clusters
-    kmeans = KMeans(n_clusters=e, random_state=0, n_init="auto").fit(player_data[['core_goals']]) 
-    # Append the inertia_ value to wss
-    wss.append(kmeans.inertia_)
